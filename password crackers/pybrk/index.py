7eccbdc19b628a3eb9fa0baecbbf7516022e09cc
wh0am123 <thejyjco@gmail.com>
2018-08-07 11:15:49 -0430

    Juan was here

--- password crackers/pybrk/index.py
+++ password crackers/pybrk/index.py
@@ -1,15 +1,26 @@
 import bcrypt;
 import base64;
 import hashlib;
-password = "super secret password"
+import random;
+from array import *;
+
+pwList = ['wsasas', 'word', 'super secret password']
+password = "super secret password".encode('utf-8')
  # Hash a password for the first time, with a randomly-generated salt
 hashed = bcrypt.hashpw(
-   base64.b64encode(hashlib.sha256(password).digest()),
-   bcrypt.gensalt()
- )
+#base64.b64encode(hashlib.sha256('super secret password'.encode('utf-8')).digest())
+'super secret password'.encode('utf-8'),
+bcrypt.gensalt()
+)
  # Check that an unhashed password matches one that has previously been
  # hashed
-if bcrypt.checkpw(password, hashed):
-     print("It Matches!")
-else:
-     print("It Does not Match :(")
+i = 0
+while not bcrypt.checkpw(pwList[i].encode('utf-8'), hashed):
+	#if bcrypt.checkpw(password, hashed):
+    #	 print("It Matches!")
+	#else:
+    print("It Does not Match :(")
+    i = i + 1
+
+print('It matches, damn!, Password: ' + str(pwList[i]))
+
